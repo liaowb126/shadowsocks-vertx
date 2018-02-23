94a42ddaad6d6a4ceca3d657157902b7887cf53c
lenovobenben <lenovobenben@gmail.com>
2018-02-23 17:52:18 +0800

    尝试打开 tcp_keepalive ，观察死链接是否会被删除 2

--- src/main/java/shadowsocks/vertxio/ServerHandler.java
+++ src/main/java/shadowsocks/vertxio/ServerHandler.java
@@ -188,7 +188,7 @@ public class ServerHandler implements Handler<Buffer> {
     }
 
     private void connectToRemote(String addr, int port) {
-        NetClientOptions options = new NetClientOptions().setConnectTimeout(this.mConfig.timeout);
+        NetClientOptions options = new NetClientOptions().setConnectTimeout(this.mConfig.timeout).setTcpKeepAlive(true);
         NetClient client = mVertx.createNetClient(options);
         client.connect(port, addr, res -> {  // connect handler
             if (!res.succeeded()) {
